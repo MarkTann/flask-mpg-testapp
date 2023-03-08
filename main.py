a9a8a930b45ba0925b98c23a37997d711d835ab3
MarkTann <114025644+MarkTann@users.noreply.github.com>
2023-03-08 20:19:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, jsonify, request
+import pickle
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return """
+    <h1>APP para calcular MPG</h1>
+    APP para testear flask y Railway
+    """
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/api/v1/predictions", methods=["GET"])
+def predictions():
+    cylinders = request.args["cylinders"]
+    diplacement = request.args["displacement"]
+    horsepower = request.args["horsepower"]
+    origin = request.args["origin"]
+    weight = request.args["weight"]
+    acceleration = request.args["acceleration"]
+    model_year = request.args["model_year"]
 
+
+    map_origin = {'usa': 1, 'japan':2, 'europe':3} # el usuario se tiene que acoplar a nuestras normas. Tiene que pasarlo asi con este formato
+    origin = request.args["origin"]
+    origin = map_origin[origin]
+
+    filename = "model.save"
+    loaded_model = pickle.load(open(filename, "rb"))
+    new_data = [cylinders,
+                diplacement,
+                horsepower,
+                weight,
+                acceleration,
+                origin,
+                model_year
+                ]
+    return jsonify(loaded_model.predict([new_data]))
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
